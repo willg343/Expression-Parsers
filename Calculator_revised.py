64de0e59b908df0c1b7f8fd6a3964f08edad3957
William Gusarov <67597085+willg343@users.noreply.github.com>
2022-11-22 19:04:22 -0400

    Create Calculator_revised.py

---
+++ Calculator_revised.py
@@ -0,0 +1,23 @@
+"""
+Changed the fully regex-based approach to expression calculation and testing, making it only a part of the replacement algorithm.
+"""
+
+import re, operator
+
+signs = {'+':operator.add, '-':operator.sub, '*': operator.mul, '/': operator.truediv}
+convert = lambda inp: re.sub(r'[+-]+', lambda signs: ('+','-')[signs[0].count('-')%2], inp.replace(' ',''))
+calculate = lambda num1, op, num2:  str(float(signs[op](float(num1), float(num2))))
+PATTERN = re.compile(r"((?:(?:(?<=^)|(?<=[*/+-]))[-+])?\d+(?:\.\d+)?(?:e-?\d+)?|[*/+-])")
+
+def process(split_exp, default_signs = '+-', idx = 0):
+    while 1:
+        if idx==len(split_exp)-1: return split_exp if default_signs == '+-' else process(split_exp)
+        if split_exp[idx+1] in default_signs:
+            split_exp[idx:idx+3] = [calculate(*split_exp[idx:idx+3])]
+            continue
+        idx+=1
+        
+def calc(exp):
+    while '(' in exp or ')' in exp:
+        exp = re.sub(r'[(]([^()]+)[)]', lambda x:''.join(process(PATTERN.findall(x[1]), '/*')), convert(exp))
+    return float(process(PATTERN.findall(convert(exp)),'/*')[0])
